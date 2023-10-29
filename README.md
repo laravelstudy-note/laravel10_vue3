6f88d1e49838d2570b52c0471153f6574c443f9d
miyazawa <ryosukemiyazawa@gmail.com>
2023-10-29 22:27:03 +0900

    laravel/ui版

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Laravel + Vue3 + Vue-Router
+
+laravel/uiバージョン
+
+```
+$ composer require laravel/ui
+$ php artisan ui vue --auth
+$ npm install
+$ npm run dev
+```
+
+home.blade.phpに下記を記述
+
+```php
+# resources/views/home.blade.php
+<div id="app">
+<example-component></example-component>
+</div>
+```
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+
+						{{ counter }}
+
+						<button @click="increment">increment</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+	import { ref } from "vue";
+
+	const counter = ref(0)
+	const increment = () => {
+		counter.value += 1;
+	}
+</script>
