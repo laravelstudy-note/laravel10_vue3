b49ec3f52e6880afd1b95157905d3f2c50daa27b
miyazawa <ryosukemiyazawa@gmail.com>
2023-10-29 22:17:58 +0900

    breezeでの初期化

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Laravel + Vue3 + Vue-Router
+
+laravel/breezeを入れるパターン
+
+```
+$ composer require laravel/breeze --dev
+$ php artisan breeze:install
+-> Vue with Inertiaを選択
+
+$ php artisan migrate
+$ npm install
+$ npm run dev
+```
+
+※ @vitejs/plugin-vueがインストールされる
+
+http://localhost:8000/register
+
+=> 会員登録を行う
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { ref,reactive } from "vue";
+
+type CounterType = {
+	counter: number
+}
+
+const counter = ref(0);
+
+const counterObj = reactive<CounterType>({
+	counter: 0
+})
+
+const increment = () => {
+	counter.value++;
+	counterObj.counter = counterObj.counter + 1
+};
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">You're logged in!</div>
+
+					<button @click="increment">counter={{ counter }}</button>
+
+					<p>{{ counterObj.counter }}</p>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
